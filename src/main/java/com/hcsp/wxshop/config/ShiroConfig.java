2ca2a66bc9e172d23895bc5d38c57a45f705528b
Mattison chao <mattisonchao@gmail.com>
2020-04-20 08:40:37 +0800

    Fixed(ShiroConfig) fixed shiro error when restart the server.
    Fix errors caused by mismatched keys after restart the server.

--- src/main/java/com/hcsp/wxshop/config/ShiroConfig.java
+++ src/main/java/com/hcsp/wxshop/config/ShiroConfig.java
@@ -6,9 +6,12 @@ import com.hcsp.wxshop.service.UserService;
 import com.hcsp.wxshop.service.VerificationCodeCheckService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +30,10 @@ import java.util.Map;
 
 @Configuration
 public class ShiroConfig implements WebMvcConfigurer {
+
+    private static final String COOKIE_NAME = "rememberMe"; //  cookie name
+
+    private static final int EXPIRY_TIME = 86400; // seconds
     @Autowired
     UserService userService;
 
@@ -76,8 +83,21 @@ public class ShiroConfig implements WebMvcConfigurer {
         securityManager.setRealm(shiroRealm);
         securityManager.setCacheManager(new MemoryConstrainedCacheManager());
         securityManager.setSessionManager(new DefaultWebSessionManager());
+        securityManager.setRememberMeManager(rememberMeManager());
         SecurityUtils.setSecurityManager(securityManager);
         return securityManager;
+    }
+
+    /**
+     * Remember me manager
+     */
+    public CookieRememberMeManager rememberMeManager() {
+        SimpleCookie cookie = new SimpleCookie(COOKIE_NAME);
+        cookie.setMaxAge(EXPIRY_TIME);
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(cookie);
+        cookieRememberMeManager.setCipherKey(Base64.decode("3AvVhmFLUs0KTA3KaTHGFg=="));  // RememberMe cookie encryption key default AES algorithm of key length (128, 256, 512)
+        return cookieRememberMeManager;
     }
 
     @Bean
